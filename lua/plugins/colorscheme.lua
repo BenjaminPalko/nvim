b5a701040e0bb854ddafa9a2c9c82796e6ba9775
Benjamin Palko <benjaminpalko@hotmail.com>
2025-05-07 23:55:57 -0400

    transparent bg

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,26 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
-  { "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000, 
-    opts = { flavour = "mocha", 
-    --transparent_background = true, 
-    }, 
+  { "neanias/everforest-nvim", name = "everforest", opts = { transparent_background = true } },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = { flavour = "mocha", transparent_background = true },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
   },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
+      -- colorscheme = "tokyonight-moon",
       colorscheme = "catppuccin",
     },
-  }
+  },
 }
