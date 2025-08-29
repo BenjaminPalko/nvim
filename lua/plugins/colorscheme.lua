87dfb667e2fa8523d06de347ebbfd947c6676aba
Benjamin Palko <benjaminpalko@hotmail.com>
2025-08-29 18:50:16 -0400

    neogyatt

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,26 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
-  { "neanias/everforest-nvim", name = "everforest", opts = { transparent_background = true } },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = { flavour = "mocha", transparent_background = true },
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = true,
-    },
-  },
+  -- { "ellisonleao/gruvbox.nvim" },
+  { "neanias/everforest-nvim", name = "everforest", opts = { transparent_background_level = 1 } },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   opts = { flavour = "mocha", transparent_background = true },
+  -- },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     transparent = true,
+  --   },
+  -- },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "tokyonight-moon",
-      colorscheme = "catppuccin",
+      colorscheme = "everforest",
     },
   },
 }
